893924694a5482027e26b314e6e26664ce11aedd
Ritsuki Goto <develop@mizucoffee.net>
2018-04-03 02:10:46 +0900

    [add] image post

--- main.js
+++ main.js
@@ -11,7 +11,6 @@ window.onload = () => {
   const create = document.getElementById("create")
   const connect = document.getElementById("connect")
   const loading = document.getElementById("loading")
-  const error = document.getElementById("error")
   const main = document.getElementById("main")
 
   fetch(base + "/session", {
@@ -26,6 +25,75 @@ window.onload = () => {
       openMain()
     }
   })
+  const upBox = document.getElementById('uploadBox');
+
+  upBox.addEventListener('dragover', function(event) {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'copy';
+    upBox.classList.add('uphover');;
+  });
+
+  upBox.addEventListener('dragleave', function(event) {
+    upBox.classList.remove('uphover');;
+  });
+
+  upBox.addEventListener('drop', function(event) {
+    event.preventDefault();
+    upBox.classList.remove('uphover');;
+
+    var files = event.dataTransfer.files;
+    console.log(files);
+
+    // 拡張子チェック
+    // エラー処理
+
+    for (var i=0, l=files.length; i<l; i++) {
+      var reader = new FileReader();
+      reader.onload = function(event) {
+        var formData  = new FormData();
+        formData.append("image",files[0])
+
+        fetch(base + "/image", {
+          method: 'POST',
+          mode: 'cors',
+          credentials: 'include',
+          body: formData
+        }).then(function(response) {
+          console.log(response)
+          return response.json();
+        }).then(function(json) {
+          console.log(json)
+          if(json.status == 200) {
+          }else if(json.status == 409){
+          }
+        });
+      };
+      reader.readAsDataURL(files[i])
+    }
+
+  });
+
+  const file = document.getElementById("file")
+  file.addEventListener('change',function(e) {
+
+    var formData  = new FormData();
+    formData.append("image",file.files[0])
+
+    fetch(base + "/image", {
+      method: 'POST',
+      mode: 'cors',
+      credentials: 'include',
+      body: formData
+    }).then(function(response) {
+      console.log(response)
+      return response.json();
+    }).then(function(json) {
+      if(json.status == 200) {
+      }else if(json.status == 409){
+      }
+    });
+  })
+
 }
 
 function banner(){
@@ -46,51 +114,75 @@ function banner(){
 }
 
 function openHome() {
-  error.classList.remove("show");
   menu.classList.add("show")
   create.classList.remove("show")
   connect.classList.remove("show")
   loading.classList.remove("show")
   main.classList.remove("show")
+  image.classList.remove("show")
+  theme.classList.remove("show")
 }
 
 function openCreateMenu() {
-  //document.getElementById("error").style.display="none"
   menu.classList.remove("show")
   create.classList.add("show");
   connect.classList.remove("show")
   loading.classList.remove("show")
   main.classList.remove("show")
+  image.classList.remove("show")
+  theme.classList.remove("show")
 }
 
 function openConnectMenu() {
-  document.getElementById("error").style.display="none"
   menu.classList.remove("show")
   create.classList.remove("show");
   connect.classList.add("show")
   loading.classList.remove("show")
   main.classList.remove("show")
+  image.classList.remove("show")
+  theme.classList.remove("show")
 }
 
 function openLoading() {
-  document.getElementById("error").style.display="none"
   menu.classList.remove("show")
   create.classList.remove("show");
   connect.classList.remove("show")
   loading.classList.add("show")
   main.classList.remove("show")
+  image.classList.remove("show")
+  theme.classList.remove("show")
 }
 
 function openMain() {
-  document.getElementById("error").style.display="none"
   menu.classList.remove("show")
   create.classList.remove("show");
   connect.classList.remove("show")
   loading.classList.remove("show")
   main.classList.add("show")
+  image.classList.remove("show")
+  theme.classList.remove("show")
+}
+function openImageMenu() {
+  menu.classList.remove("show")
+  create.classList.remove("show");
+  connect.classList.remove("show")
+  loading.classList.remove("show")
+  main.classList.remove("show")
+  image.classList.add("show")
+  theme.classList.remove("show")
+}
+function openThemeMenu() {
+  menu.classList.remove("show")
+  create.classList.remove("show");
+  connect.classList.remove("show")
+  loading.classList.remove("show")
+  main.classList.remove("show")
+  image.classList.remove("show")
+  theme.classList.add("show")
 }
 
 function serverCreate(){
+  const error = document.getElementById("error-cre")
   const name = document.getElementById("name-cre").value
   const pass = document.getElementById("pass-cre").value
   if(name == "" || pass == "") {
@@ -98,7 +190,7 @@ function serverCreate(){
     error.innerText = "Error: This Server Name and Password is required"
     return
   }
-  document.getElementById("error").style.display="none"
+  error.style.display="none"
   openLoading()
   fetch(base + "/server", {
     method: 'POST',
@@ -120,6 +212,7 @@ function serverCreate(){
 }
 
 function sessionLogin(){
+  const error = document.getElementById("error-con")
   const name = document.getElementById("name-con").value
   const pass = document.getElementById("pass-con").value
   if(name == "" || pass == "") {
@@ -127,7 +220,7 @@ function sessionLogin(){
     error.classList.add("show");
     return
   }
-  document.getElementById("error").style.display="none"
+  error.style.display="none"
   openLoading()
   fetch(base + "/session", {
     method: 'POST',
@@ -170,14 +263,19 @@ function checkForm(f) {
 }
 
 function GetCookies(){
-    var result = new Array();
-    var allcookies = document.cookie;
-    if( allcookies != '' ){
-        var cookies = allcookies.split( '; ' );
-        for( var i = 0; i < cookies.length; i++ ){
-            var cookie = cookies[ i ].split( '=' );
-            result[ cookie[ 0 ] ] = decodeURIComponent( cookie[ 1 ] );
-        }
+  var result = new Array();
+  var allcookies = document.cookie;
+  if( allcookies != '' ){
+    var cookies = allcookies.split( '; ' );
+    for( var i = 0; i < cookies.length; i++ ){
+      var cookie = cookies[ i ].split( '=' );
+      result[ cookie[ 0 ] ] = decodeURIComponent( cookie[ 1 ] );
     }
+  }
   return result;
+}
+
+function upload(){
+  const f = document.getElementById("file")
+  f.click()
 }
